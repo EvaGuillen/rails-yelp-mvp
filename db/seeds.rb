829e1fec44ac9c2f3a41366e699628602b868828
Eva Guillen <eva.guillen6@gmail.com>
2019-11-06 17:20:44 +0100

    exercice 2 v2 (yelp mvp) advanced routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Fourchette',
+    address:      '5 rue Clotilde Bizolon 69002 Lyon',
+    phone_number: '0485760645',
+    category:     'french'
+  },
+  {
+    name:         'Couteau',
+    address:      '5 rue Clotilde Bizolon 69002 Lyon',
+    phone_number: '0485760645',
+    category:     'chinese'
+  },
+  {
+    name:         'Cuillère',
+    address:      '5 rue Clotilde Bizolon 69002 Lyon',
+    phone_number: '0485760645',
+    category:     'italian'
+  },
+   {
+    name:         'Assiette',
+    address:      '5 rue Clotilde Bizolon 69002 Lyon',
+    phone_number: '0485760645',
+    category:     'belgian'
+  },
+   {
+    name:         'Bol',
+    address:      '5 rue Clotilde Bizolon 69002 Lyon',
+    phone_number: '0485760645',
+    category:     'french'
+  }
+]
+  Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
